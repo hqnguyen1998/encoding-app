5dc0f5d109e69b050a671fa0845aa1ffeeee29c9
hqnguyen1998 <hqnguyen1998@gmail.com>
2026-08-16 23:56:05 -0400

    Allow direct upload regression test time

--- electron/onzload/upload.test.ts
+++ electron/onzload/upload.test.ts
@@ -79,5 +79,5 @@ describe('OnzLoad direct upload', () => {
     } finally {
       await new Promise<void>((resolve, reject) => server.close((error) => error ? reject(error) : resolve()));
     }
-  });
+  }, 20_000);
 });
